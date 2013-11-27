d1f223b58fd8821a185834c0873eb21d68be7ab8
proller <proler@github.com>
2013-11-27 19:03:18 +0400

    Good default settings

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -288,6 +288,20 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("high_precision_fpu", "true");
 
 	settings->setDefault("language", "");
+
+	// freeminer user-friendly defaults
+	settings->setDefault("enable_vbo", "true");
+	settings->setDefault("viewing_range_nodes_max", "1000");
+	settings->setDefault("mg_name", "indev");
+	settings->setDefault("mg_flags", "trees, caves, v6_biome_blend, v6_jungles, dungeons");
+	settings->setDefault("liquid_finite", "true");
+	settings->setDefault("weather", "true");
+	settings->setDefault("ipv6_server", "true");
+	settings->setDefault("max_users", "100");
+	settings->setDefault("server_map_save_interval", "60");
+	settings->setDefault("max_block_send_distance", "30");
+	settings->setDefault("public_serverlist", "1");
+	settings->setDefault("main_menu_tab", "multiplayer");
 }
 
 void override_default_settings(Settings *settings, Settings *from)
