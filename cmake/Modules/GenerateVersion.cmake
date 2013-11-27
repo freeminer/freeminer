a268f381b08c476e969fd7aba5e0f63bc2a3a1a3
proller <proler@github.com>
2013-11-27 20:29:50 +0400

    Very long version like   0.4.8.0-free-e4eae79

--- cmake/Modules/GenerateVersion.cmake
+++ cmake/Modules/GenerateVersion.cmake
@@ -3,12 +3,14 @@
 if(VERSION_EXTRA)
 	set(VERSION_GITHASH "${VERSION_STRING}")
 else(VERSION_EXTRA)
-	execute_process(COMMAND git describe --always --tag --dirty
+	#execute_process(COMMAND git describe --always --tag --dirty
+	execute_process(COMMAND git describe --always --dirty
 		WORKING_DIRECTORY "${GENERATE_VERSION_SOURCE_DIR}"
 		OUTPUT_VARIABLE VERSION_GITHASH OUTPUT_STRIP_TRAILING_WHITESPACE
 		ERROR_QUIET)
 
 	if(VERSION_GITHASH)
+		set(VERSION_GITHASH "${VERSION_STRING}-${VERSION_GITHASH}")
 		message(STATUS "*** Detected git version ${VERSION_GITHASH} ***")
 	else()
 		set(VERSION_GITHASH "${VERSION_STRING}")
