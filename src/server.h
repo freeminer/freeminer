ecfc98452b64669c904aba1925e837a18caada96
proller <proller@github.com>
2013-12-22 04:24:48 +0400

    Experimental: send far blocks as non reliable
    Squashed commit of the following:
    
    commit 6b3062c9b06828119d1c402306dbda2e01da342c
    Merge: f8de09d 446ea37
    Author: proller <proller@github.com>
    Date:   Sun Dec 22 04:23:30 2013 +0400
    
        Merge remote-tracking branch 'upstream/master' into lag
    
    commit f8de09d0b2f7366c759380b275556124006ad17b
    Author: proller <proller@github.com>
    Date:   Sun Dec 22 03:45:23 2013 +0400
    
        far non reliable blocks
    
    commit 0e90dc44701428ffb2837c1f8799e8f828b45b7e
    Author: proller <proller@github.com>
    Date:   Fri Dec 20 02:35:16 2013 +0400
    
        Experimental: send far blocks as non reliable
    
    commit b5fff347151966ecb471ff837e3f9ed56eb0b317
    Author: proller <proller@github.com>
    Date:   Fri Dec 20 01:07:12 2013 +0400
    
        tune
    
    commit c6b13e71bb925409091004d2abea1628fc80d8ef
    Author: proller <proller@github.com>
    Date:   Fri Dec 20 00:23:17 2013 +0400
    
        still tuning
    
    commit 4ed7aaa0abe6b6b5c4d4d0adf2276b4863c0900e
    Author: proller <proller@github.com>
    Date:   Fri Dec 20 00:10:25 2013 +0400
    
        Tuning for slow mobile conections
    
    commit 9e7bfcc3c8269552cb0b4cbe3aeab596dcb9a18c
    Author: proller <proller@github.com>
    Date:   Thu Dec 19 23:31:58 2013 +0400
    
        Connecction tune

--- src/server.h
+++ src/server.h
@@ -539,7 +539,7 @@ private:
 	void setBlockNotSent(v3s16 p);
 
 	// Environment and Connection must be locked when called
-	void SendBlockNoLock(u16 peer_id, MapBlock *block, u8 ver, u16 net_proto_version);
+	void SendBlockNoLock(u16 peer_id, MapBlock *block, u8 ver, u16 net_proto_version, bool reliable = 1);
 
 	// Sends blocks to clients (locks env and con on its own)
 	void SendBlocks(float dtime);
