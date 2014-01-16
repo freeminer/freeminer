3270cdbfe5c353a1a0b12f290ee8d82600e2e594
proller <proller@github.com>
2014-01-16 22:05:10 +0400

    Disable ipv6

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -347,7 +347,7 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("sqlite_synchronous", "1");
 
 #ifndef _WIN32
-	settings->setDefault("ipv6_server", "true"); // problems on all windows versions (unable to play in local game)
+//enet! TODO	settings->setDefault("ipv6_server", "true"); // problems on all windows versions (unable to play in local game)
 #endif
 }
 
