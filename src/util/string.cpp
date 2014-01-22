15b89b99c7640c57b299388ca37470910e3688de
Ilya Zhuravlev <zhuravlevilya@ya.ru>
2014-01-23 01:19:57 +0400

    UTF8 conversion implementation for Windows.

--- src/util/string.cpp
+++ src/util/string.cpp
@@ -30,9 +30,13 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "../hex.h"
 #include "../porting.h"
 
+#ifndef _WIN32
 #include <iconv.h>
-#include <errno.h>
+#else
+#include <Windows.h>
+#endif
 
+#ifndef _WIN32
 size_t convert(const char *to, const char *from, char *outbuf, size_t outbuf_size, char *inbuf, size_t inbuf_size) {
 	iconv_t cd = iconv_open(to, from);
 
@@ -86,6 +90,27 @@ std::string wide_to_utf8(const std::wstring &input) {
 
 	return out;
 }
+#else
+std::wstring utf8_to_wide(const std::string &input) {
+	size_t outbuf_size = input.size() + 1;
+	wchar_t *outbuf = new wchar_t[outbuf_size];
+	memset(outbuf, 0, outbuf_size * sizeof(wchar_t));
+	MultiByteToWideChar(CP_UTF8, 0, input.c_str(), input.size(), outbuf, outbuf_size);
+	std::wstring out(outbuf);
+	delete[] outbuf;
+	return out;
+}
+
+std::string wide_to_utf8(const std::wstring &input) {
+	size_t outbuf_size = (input.size() + 1) * 6;
+	char *outbuf = new char[outbuf_size];
+	memset(outbuf, 0, outbuf_size);
+	WideCharToMultiByte(CP_UTF8, 0, input.c_str(), input.size(), outbuf, outbuf_size, NULL, NULL);
+	std::string out(outbuf);
+	delete[] outbuf;
+	return out;
+}
+#endif
 
 std::wstring narrow_to_wide(const std::string& mbs)
 {
