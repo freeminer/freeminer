d49dbf2f30f3f409229ab20dbeff7d5720b4fede
proller <proller@github.com>
2014-06-18 19:35:38 +0400

    print json searialize error in kv_put

--- builtin/key_value_storage.lua
+++ builtin/key_value_storage.lua
@@ -3,19 +3,24 @@
 --
 
 function freeminer.kv_put(key, data)
-   freeminer.kv_put_string(key, freeminer.write_json(data))
+    local json = freeminer.write_json(data)
+    if not json then
+        core.log("error", "kv_put: Error in json serialize key=".. key .. " luaized_data=" .. core.serialize(data))
+        return
+    end
+    return freeminer.kv_put_string(key, json)
 end
 
 function freeminer.kv_get(key)
-   local data = freeminer.kv_get_string(key)
-   if data ~= nil then
-   	  data = freeminer.parse_json(data)
-   end
-   return data
+    local data = freeminer.kv_get_string(key)
+    if data ~= nil then
+        data = freeminer.parse_json(data)
+    end
+    return data
 end
 
 function freeminer.kv_rename(key1, key2)
-   local data = freeminer.kv_get_string(key1)
-   freeminer.kv_delete(key1)
-   freeminer.kv_put_string(key2, data)
+    local data = freeminer.kv_get_string(key1)
+    freeminer.kv_delete(key1)
+    freeminer.kv_put_string(key2, data)
 end
