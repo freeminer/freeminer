ef45fec754eed5d1813c0e034785d6431d16c58d
Ilya Zhuravlev <whatever@xyz.is>
2014-07-04 01:55:43 +0400

    Add FORCE_LEVELDB because with nuget we don't have leveldb lib/include.
    Conflicts:
    
    	build/windows/build.py
    	src/CMakeLists.txt

--- build/windows/build.py
+++ build/windows/build.py
@@ -282,6 +282,7 @@ def main():
 		-DGETTEXT_MSGFMT=C:\usr\bin\msgfmt.exe
 		-DENABLE_GETTEXT=1
 		-DENABLE_LEVELDB=1
+		-DFORCE_LEVELDB=1
 	""".format(
 		curl_lib="libcurl_a.lib" if build_type != "Debug" else "libcurl_a_debug.lib",
 		freetype_lib="freetype252MT.lib" if build_type != "Debug" else "freetype252MT_D.lib",
