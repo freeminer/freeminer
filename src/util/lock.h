535777d8ced1bb68b7441dded4a7a8c2ebea2f83
proller <proller@github.com>
2014-07-16 19:59:49 +0400

    split lock.h to lock.cpp

--- src/util/lock.h
+++ src/util/lock.h
@@ -1,4 +1,3 @@
-
 #ifndef UTIL_LOCK_HEADER
 #define UTIL_LOCK_HEADER
 
@@ -15,6 +14,7 @@ typedef boost::shared_mutex try_shared_mutex;
 typedef boost::shared_lock<try_shared_mutex> try_shared_lock;
 typedef boost::unique_lock<try_shared_mutex> unique_lock;
 #define DEFER_LOCK boost::defer_lock
+#define LOCK_TWO 1
 
 #elif CMAKE_HAVE_SHARED_MUTEX
 //#elif __cplusplus >= 201305L
@@ -24,6 +24,7 @@ typedef std::shared_timed_mutex try_shared_mutex;
 typedef std::shared_lock<try_shared_mutex> try_shared_lock;
 typedef std::unique_lock<try_shared_mutex> unique_lock;
 #define DEFER_LOCK std::defer_lock
+#define LOCK_TWO 1
 
 #else
 
@@ -32,7 +33,6 @@ typedef std::mutex try_shared_mutex;
 typedef std::unique_lock<try_shared_mutex> try_shared_lock;
 typedef std::unique_lock<try_shared_mutex> unique_lock;
 #define DEFER_LOCK std::defer_lock
-
 #endif
 
 
@@ -68,22 +68,8 @@ public:
 	T & lock;
 	std::atomic_int &r;
 	std::atomic<std::size_t> & thread_id;
-	lock_rec(T & lock_, std::atomic_int & r_, std::atomic<std::size_t> & thread_id_):
-		lock(lock_),
-		r(r_),
-		thread_id(thread_id_) {
-		auto thread_me = std::hash<std::thread::id>()(std::this_thread::get_id());
-		if(!r || thread_me != thread_id) {
-			lock.lock();
-			thread_id = thread_me;
-		}
-		++r;
-	}
-	~lock_rec() {
-		if(!--r) {
-			//lock.unlock();
-		}
-	}
+	lock_rec(T & lock_, std::atomic_int & r_, std::atomic<std::size_t> & thread_id_);
+	~lock_rec();
 };
 
 class locker {
@@ -93,35 +79,19 @@ public:
 	std::atomic_int r;
 	std::atomic<std::size_t> thread_id;
 
-	locker() {
-		r = 0;
-	}
-
-	unique_lock lock_unique() {
-		return unique_lock(mtx);
-	}
-
-	try_shared_lock lock_shared() {
-		return try_shared_lock(mtx);
-	}
-
-	lock_rec<unique_lock> lock_unique_rec() {
-		auto lock = unique_lock(mtx, DEFER_LOCK);
-		return lock_rec<unique_lock> (lock, r, thread_id);
-	}
-
-	lock_rec<try_shared_lock> lock_shared_rec() {
-		auto lock = try_shared_lock(mtx, DEFER_LOCK);
-		return lock_rec<try_shared_lock> (lock, r, thread_id);
-	}
+	locker();
+	unique_lock lock_unique();
+	try_shared_lock lock_shared();
+	lock_rec<unique_lock> lock_unique_rec();
+	lock_rec<try_shared_lock> lock_shared_rec();
 };
 
 
 #include <map>
-template <class Key, class T, class Compare = std::less<Key>,
-          class Allocator = std::allocator<std::pair<const Key, T>>>
-                  class shared_map: public std::map<Key, T, Compare, Allocator>,
-public locker {
+template < class Key, class T, class Compare = std::less<Key>,
+         class Allocator = std::allocator<std::pair<const Key, T> >>
+class shared_map: public std::map<Key, T, Compare, Allocator>,
+	public locker {
 public:
 
 	typedef Key                                      key_type;
