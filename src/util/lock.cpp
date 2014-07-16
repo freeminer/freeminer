4de1b1262b2f4140c8da8bcdbbccb069a6bfcd65
proller <proller@github.com>
2014-07-17 02:49:12 +0400

    Locker fixes

--- src/util/lock.cpp
+++ src/util/lock.cpp
@@ -1,46 +1,54 @@
 #include "lock.h"
 
+#include "../log.h"
+
 template<class T>
-lock_rec<T>::lock_rec(T & lock_, std::atomic_int & r_, std::atomic<std::size_t> & thread_id_):
+lock_rec<T>::lock_rec(T * lock_, std::atomic_int & r_, std::atomic<std::size_t> & thread_id_):
 	lock(lock_),
 	r(r_),
 	thread_id(thread_id_) {
 	auto thread_me = std::hash<std::thread::id>()(std::this_thread::get_id());
-	if(!r || thread_me != thread_id) {
-		lock.lock();
+	//infostream<<" lock="<<lock->mutex()<<" thread_me="<<thread_me<<" thread_id="<<thread_id<<" r="<<r<<std::endl;
+	if(!r++ || thread_me != thread_id) {
+		lock->lock();
 		thread_id = thread_me;
 	}
-	++r;
 }
 
 template<class T>
 lock_rec<T>::~lock_rec() {
-	if(!--r) {
-		//lock.unlock();
+	//auto thread_me = std::hash<std::thread::id>()(std::this_thread::get_id());
+	//infostream<<" unlock="<<lock->mutex()<<" thread_me="<<thread_me<<" thread_id="<<thread_id<<" r="<<r<<std::endl;
+	if(!--r /*&& thread_me == thread_id*/) {
+		//lock->unlock();
 	}
+	delete lock;
 }
 
 
 locker::locker() {
 	r = 0;
+	//infostream<<" locker() r="<<r<<" mtx="<<&mtx<<" thread_id="<<thread_id<<std::endl;
 }
 
 unique_lock locker::lock_unique() {
+	//infostream<<" locker::lock_unique() r="<<r<<" mtx="<<&mtx<<" thread_id="<<thread_id<<std::endl;
 	return unique_lock(mtx);
 }
 
 try_shared_lock locker::lock_shared() {
+	//infostream<<" locker::lock_shared() r="<<r<<" mtx="<<&mtx<<" thread_id="<<thread_id<<std::endl;
 	return try_shared_lock(mtx);
 }
 
 lock_rec<unique_lock> locker::lock_unique_rec() {
-	auto lock = unique_lock(mtx, DEFER_LOCK);
-	return lock_rec<unique_lock> (lock, r, thread_id);
+	//infostream<<" locker::lock_unique_rec() r="<<r<<" mtx="<<&mtx<<" thread_id="<<thread_id<<std::endl;
+	return lock_rec<unique_lock> (new unique_lock(mtx, DEFER_LOCK), r, thread_id);
 }
 
 lock_rec<try_shared_lock> locker::lock_shared_rec() {
-	auto lock = try_shared_lock(mtx, DEFER_LOCK);
-	return lock_rec<try_shared_lock> (lock, r, thread_id);
+	//infostream<<" locker::lock_shared_rec() r="<<r<<" mtx="<<&mtx<<" thread_id="<<thread_id<<std::endl;
+	return lock_rec<try_shared_lock> (new try_shared_lock(mtx, DEFER_LOCK), r, thread_id);
 }
 
 
--- src/util/lock.h
+++ src/util/lock.h
@@ -65,10 +65,10 @@ public:
 template<class T>
 class lock_rec {
 public:
-	T & lock;
+	T * lock;
 	std::atomic_int &r;
 	std::atomic<std::size_t> & thread_id;
-	lock_rec(T & lock_, std::atomic_int & r_, std::atomic<std::size_t> & thread_id_);
+	lock_rec(T * lock_, std::atomic_int & r_, std::atomic<std::size_t> & thread_id_);
 	~lock_rec();
 };
 
@@ -103,27 +103,27 @@ public:
 	typedef typename full_type::iterator iterator;
 
 	mapped_type& get(const key_type& k) {
-		auto lock = lock_shared();
+		auto lock = lock_shared_rec();
 		return full_type::operator[](k);
 	}
 
 	void set(const key_type& k, const mapped_type& v) {
-		auto lock = lock_unique();
+		auto lock = lock_unique_rec();
 		full_type::operator[](k) = v;
 	}
 
 	bool      empty() {
-		auto lock = lock_shared();
+		auto lock = lock_shared_rec();
 		return full_type::empty();
 	}
 
 	size_type size() {
-		auto lock = lock_shared();
+		auto lock = lock_shared_rec();
 		return full_type::size();
 	}
 
 	size_type count(const key_type& k) {
-		auto lock = lock_shared();
+		auto lock = lock_shared_rec();
 		return full_type::count(k);
 	}
 
@@ -132,19 +132,19 @@ public:
 	mapped_type& operator[](key_type&& k) = delete;
 
 	typename full_type::iterator  erase(const_iterator position) {
-		auto lock = lock_unique();
+		auto lock = lock_unique_rec();
 		return full_type::erase(position);
 	}
 
 	size_type erase(const key_type& k) {
-		auto lock = lock_unique();
+		auto lock = lock_unique_rec();
 		return full_type::erase(k);
 	}
 
 	// iterator  erase(const_iterator first, const_iterator last);
 
 	void clear() {
-		auto lock = lock_unique();
+		auto lock = lock_unique_rec();
 		full_type::clear();
 	}
 };
