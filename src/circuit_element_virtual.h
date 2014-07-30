5ad7c935318343d20b1682260e02c4728b3bbed5
selat <longexampletestname@gmail.com>
2014-07-30 21:30:51 +0300

    Add license headers

--- src/circuit_element_virtual.h
+++ src/circuit_element_virtual.h
@@ -1,3 +1,20 @@
+/*
+  This file is part of Freeminer.
+
+  Freeminer is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Freeminer  is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Freeminer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef CIRCUIT_ELEMENT_VIRTUAL_H
 #define CIRCUIT_ELEMENT_VIRTUAL_H
 
