c6002a1d060a5823c29f51471183501e309f9926
selat <longexampletestname@gmail.com>
2014-07-31 20:50:17 +0300

    Add more circuit elements

--- games/minimal/mods/circuit/init.lua
+++ games/minimal/mods/circuit/init.lua
@@ -1,23 +1,96 @@
+-- Craft recipes and textures are from mesecons mod
 
-minetest.register_node("circuit:logic_gate", {
-	description = "Gate",
+minetest.register_node("circuit:gate_not", {
+	description = "NOT Gate",
 	circuit_states = {16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
 	                  16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
 	                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	tiles = {"default_logic_gate.png", "default_logic_gate_side.png"},
+	tiles = {"circuit_gate_not.png", "circuit_gate_bottom.png", "circuit_gate_side.png"},
 	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
 	paramtype2 = "facedir",
 	sounds = default.node_sound_stone_defaults(),
 })
 
-minetest.register_node("circuit:logic_source", {
+minetest.register_node("circuit:gate_and", {
+	description = "AND Gate",
+	circuit_states = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 16, 16, 16,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 16, 16, 16,
+	                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 16, 16, 16,
+	                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 16, 16, 16},
+	tiles = {"circuit_gate_and.png", "circuit_gate_bottom.png", "circuit_gate_side.png"},
+	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
+	paramtype2 = "facedir",
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("circuit:gate_nand", {
+	description = "NAND Gate",
+	circuit_states = {16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 0, 0, 0,
+	                  16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 0, 0, 0,
+	                  16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 0, 0, 0,
+	                  16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 0, 0, 0},
+	tiles = {"circuit_gate_nand.png", "circuit_gate_bottom.png", "circuit_gate_side.png"},
+	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
+	paramtype2 = "facedir",
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("circuit:gate_or", {
+	description = "OR Gate",
+	circuit_states = {0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+	                  0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+	                  0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+	                  0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16},
+	tiles = {"circuit_gate_or.png", "circuit_gate_bottom.png", "circuit_gate_side.png"},
+	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
+	paramtype2 = "facedir",
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("circuit:gate_nor", {
+	description = "NOR Gate",
+	circuit_states = {16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	                  16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	                  16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	                  16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	tiles = {"circuit_gate_nor.png", "circuit_gate_bottom.png", "circuit_gate_side.png"},
+	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
+	paramtype2 = "facedir",
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("circuit:gate_xor", {
+	description = "XOR Gate",
+	circuit_states = {0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16, 16, 0, 0, 0, 0,
+	                  0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16, 16, 0, 0, 0, 0,
+	                  0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16, 16, 0, 0, 0, 0,
+	                  0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16, 16, 0, 0, 0, 0},
+	tiles = {"circuit_gate_xor.png", "circuit_gate_bottom.png", "circuit_gate_side.png"},
+	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
+	paramtype2 = "facedir",
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("circuit:gate_xnor", {
+	description = "XNOR Gate",
+	circuit_states = {16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 0, 0, 16, 16, 16, 16,
+	                  16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 0, 0, 16, 16, 16, 16,
+	                  16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 0, 0, 16, 16, 16, 16,
+	                  16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 0, 0, 16, 16, 16, 16},
+	tiles = {"circuit_gate_xor.png", "circuit_gate_bottom.png", "circuit_gate_side.png"},
+	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
+	paramtype2 = "facedir",
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("circuit:source", {
 	description = "Source",
 	circuit_states = {63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
 	                  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
 	                  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
 	                  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63},
-	tiles = {"default_logic_source.png", "default_logic_source_side.png"},
+	tiles = {"circuit_source.png", "circuit_source.png", "circuit_source_side.png"},
 	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
 	sounds = default.node_sound_stone_defaults(),
 })
@@ -25,7 +98,7 @@ minetest.register_node("circuit:logic_source", {
 minetest.register_node("circuit:wire", {
 	description = "Wire",
 	is_wire = true,
-	tiles = {"default_wire.png", "default_wire_side.png"},
+	tiles = {"circuit_wire.png", "circuit_wire.png", "circuit_wire_side.png"},
 	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
 	sounds = default.node_sound_stone_defaults(),
 })
@@ -36,7 +109,7 @@ minetest.register_node("circuit:wire_connector", {
 	is_wire = true,
 	is_connector = true,
 	wire_connections = {2, 1, 8, 4, 32, 16},
-	tiles = {"default_wire2.png", "default_wire2_side.png"},
+	tiles = {"circuit_wire2.png", "circuit_wire2.png", "circuit_wire2_side.png"},
 	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
 	sounds = default.node_sound_stone_defaults(),
 })
@@ -53,7 +126,7 @@ minetest.register_node("circuit:lever", {
 		minetest.swap_node(pos, {name = "circuit:active_lever",
 		                   param1 = node.param1, param2 = node.param2})
 	end,
-	tiles = {"default_lever.png", "default_lever_side.png"},
+	tiles = {"circuit_lever.png", "circuit_lever.png", "circuit_lever_side.png"},
 	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
 	sounds = default.node_sound_stone_defaults(),
 })
@@ -69,7 +142,8 @@ minetest.register_node("circuit:active_lever", {
 		minetest.swap_node(pos, {name = "circuit:lever",
 		                   param1 = node.param1, param2 = node.param2})
 	end,
-	tiles = {"default_active_lever.png", "default_active_lever_side.png"},
+	drop = "circuit:lever",
+	tiles = {"circuit_active_lever.png", "circuit_active_lever.png", "circuit_active_lever_side.png"},
 	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
 	sounds = default.node_sound_stone_defaults(),
 })
@@ -81,7 +155,7 @@ minetest.register_node("circuit:lamp", {
 		node.name = "circuit:active_lamp"
 		minetest.swap_node(pos, node)
 	end,
-	tiles = {"default_lamp.png"},
+	tiles = {"circuit_lamp.png"},
 	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
 	sounds = default.node_sound_stone_defaults(),
 })
@@ -93,7 +167,125 @@ minetest.register_node("circuit:active_lamp", {
 		minetest.swap_node(pos, {name = "circuit:lamp",
 		                   param1 = node.param1, param2 = node.param2})
 	end,
-	tiles = {"default_active_lamp.png"},
+	drop = "circuit:lamp",
+	tiles = {"circuit_active_lamp.png"},
 	groups = {snappy=2,choppy=3,oddly_breakable_by_hand=2,dig_immediate=3},
 	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_craftitem("circuit:silicon", {
+	image = "jeija_silicon.png",
+	on_place_on_ground = minetest.craftitem_place_item,
+    	description="Silicon",
+})
+
+minetest.register_craft({
+	output = "circuit:silicon 4",
+	recipe = {
+		{"default:sand", "default:sand"},
+		{"default:sand", "default:steel_ingot"},
+	}
+})
+
+minetest.register_craft({
+		output = "circuit:wire 16",
+		recipe = {
+			   {"default:wood", "default:wood", "default:wood"},
+			   {"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
+			   {"default:wood", "default:wood", "default:wood"},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:wire_connector 16",
+		recipe = {
+			   {"default:wood", "default:steel_ingot", "default:wood"},
+			   {"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
+			   {"default:wood", "default:steel_ingot", "default:wood"},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:source 4",
+		recipe = {
+			   {"circuit:silicon", "circuit:silicon"},
+			   {"circuit:silicon", "circuit:silicon"},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:lever 4",
+		recipe = {
+			   {"default:stick", "default:wood"},
+			   {"circuit:silicon", "default:steel_ingot"},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:lamp 4",
+		recipe = {
+			   {"default:wood", "circuit:silicon"},
+			   {"circuit:silicon", "default:wood"},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:gate_and 4",
+		recipe = {
+			   {"circuit:silicon"},
+			   {"circuit:silicon"},
+			   {"circuit:silicon"},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:gate_nand 4",
+		recipe = {
+			   {"circuit:silicon", ""},
+			   {"circuit:silicon", "circuit:silicon"},
+			   {"circuit:silicon", ""},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:gate_or 4",
+		recipe = {
+			   {"circuit:silicon"},
+			   {""},
+			   {"circuit:silicon"},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:gate_nor 4",
+		recipe = {
+			   {"circuit:silicon", ""},
+			   {"", "circuit:silicon"},
+			   {"circuit:silicon", ""},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:gate_xor 4",
+		recipe = {
+			   {"", "circuit:silicon"},
+			   {"circuit:silicon", ""},
+			   {"", "circuit:silicon"},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:gate_xnor 4",
+		recipe = {
+			   {"", "circuit:silicon"},
+			   {"circuit:silicon", "circuit:silicon"},
+			   {"", "circuit:silicon"},
+		}
+})
+
+minetest.register_craft({
+		output = "circuit:gate_not 4",
+		recipe = {
+			   {"circuit:silicon", "circuit:silicon"},
+		}
 })
