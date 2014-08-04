0d3edd0b7b20371e7fb09e8ded9ef8978e2c38a1
proller <proller@github.com>
2014-08-04 20:11:26 +0400

    Small fixes for android build (WIP)

--- build/android/jni/Application.mk
+++ build/android/jni/Application.mk
@@ -1,8 +1,8 @@
-# NDK_TOOLCHAIN_VERSION := clang3.3
+NDK_TOOLCHAIN_VERSION := clang3.4
 
 APP_PLATFORM := android-9
 APP_MODULES := minetest
 APP_STL := gnustl_static
 
-APP_CPPFLAGS += -fexceptions
+APP_CPPFLAGS += -std=c++0x -fexceptions
 APP_GNUSTL_FORCE_CPP_FEATURES := rtti
