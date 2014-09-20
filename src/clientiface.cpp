8a0dd4ad20070ec053465403e7a62403e76c40db
proller <proller@github.com>
2014-09-20 22:51:15 +0400

    better m_blocks_sent lock

--- src/clientiface.cpp
+++ src/clientiface.cpp
@@ -371,12 +371,13 @@ int RemoteClient::GetNextBlocks(
 			/*
 				Don't send already sent blocks
 			*/
-			auto block_sent = m_blocks_sent.find(p) != m_blocks_sent.end() ? m_blocks_sent.get(p) : 0;
+			unsigned int block_sent;
 			{
-				if(block_sent > 0 && block_sent + (d <= 2 ? 1 : d*d*d) > m_uptime) {
-					continue;
-				}
+				auto lock = m_blocks_sent.lock_shared_rec();
+				block_sent = m_blocks_sent.find(p) != m_blocks_sent.end() ? m_blocks_sent.get(p) : 0;
 			}
+			if(block_sent > 0 && block_sent + (d <= 2 ? 1 : d*d*d) > m_uptime)
+				continue;
 
 			/*
 				Check if map has this block
