817084dfd868eb77a90c4be32b17b026d5be1797
proller <proller@github.com>
2014-10-14 18:32:11 +0400

    locked correctBlockNodeIds

--- src/mapblock.cpp
+++ src/mapblock.cpp
@@ -40,7 +40,7 @@ along with Freeminer.  If not, see <http://www.gnu.org/licenses/>.
 #include "util/serialize.h"
 #include "circuit.h"
 #include "profiler.h"
-#include "main.h"
+#include <mutex>
 
 #define PP(x) "("<<(x).X<<","<<(x).Y<<","<<(x).Z<<")"
 
@@ -481,6 +481,7 @@ static void getBlockNodeIdMapping(NameIdMapping *nimap, MapNode *nodes,
 // Correct ids in the block to match nodedef based on names.
 // Unknown ones are added to nodedef.
 // Will not update itself to match id-name pairs in nodedef.
+static std::mutex correctBlockNodeIds_mutex;
 static void correctBlockNodeIds(const NameIdMapping *nimap, MapNode *nodes,
 		IGameDef *gamedef)
 {
@@ -491,6 +492,7 @@ static void correctBlockNodeIds(const NameIdMapping *nimap, MapNode *nodes,
 	// correct ids.
 	std::set<content_t> unnamed_contents;
 	std::set<std::string> unallocatable_contents;
+	std::lock_guard<std::mutex> lock(correctBlockNodeIds_mutex);
 	for(u32 i=0; i<MAP_BLOCKSIZE*MAP_BLOCKSIZE*MAP_BLOCKSIZE; i++)
 	{
 		content_t local_id = nodes[i].getContent();
