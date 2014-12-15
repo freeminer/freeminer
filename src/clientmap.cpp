68dd9d2e311dc89e324835d5623c86578ca52152
proller <proller@github.com>
2014-12-15 20:33:54 +0300

    Revert "fix build"
    This reverts commit 21e53593d3cc3e84cd9fb7247da4be0a6c90b002.

--- src/clientmap.cpp
+++ src/clientmap.cpp
@@ -53,6 +53,7 @@ MapDrawControl::MapDrawControl():
 		,fps_avg(30)
 		,fps_wanted(30)
 		,drawtime_avg(30)
+		,block_overflow(false)
 	{
 		farmesh = g_settings->getS32("farmesh");
 		farmesh_step = g_settings->getS32("farmesh_step");
