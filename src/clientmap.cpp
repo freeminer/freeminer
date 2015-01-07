d767eb171370aed296a3eee807e32107589b42c9
proller <proller@github.com>
2015-01-07 22:09:07 +0300

    fix postfx flicker

--- src/clientmap.cpp
+++ src/clientmap.cpp
@@ -931,9 +931,7 @@ void ClientMap::renderPostFx(CameraMode cam_mode)
 	v3f camera_position = m_camera_position;
 	m_camera_mutex.Unlock();
 
-	MapNode n = getNodeTry(floatToInt(camera_position, BS));
-	if (n.getContent() == CONTENT_IGNORE)
-		return; // may flicker
+	MapNode n = getNodeNoEx(floatToInt(camera_position, BS));
 
 	// - If the player is in a solid node, make everything black.
 	// - If the player is in liquid, draw a semi-transparent overlay.
