8eb2b98f1b6685144834b290b523fba49f640a83
proller <proller@github.com>
2015-01-15 22:04:05 +0300

    reduce merge conflicts in map

--- src/fm_map.cpp
+++ src/fm_map.cpp
@@ -19,10 +19,13 @@ along with Freeminer.  If not, see <http://www.gnu.org/licenses/>.
 #include "mapblock.h"
 #include "log_types.h"
 #include "util/lock.h"
-
-//#include "config.h"
 #include "profiler.h"
-//#include "porting.h"
+
+#include "nodedef.h"
+#include "environment.h"
+#include "emerge.h"
+#include "mg_biome.h"
+#include "gamedef.h"
 
 #if CMAKE_HAVE_THREAD_LOCAL
 thread_local MapBlockP m_block_cache = nullptr;
@@ -135,4 +138,165 @@ void Map::deleteBlock(MapBlockP block)
 	m_block_cache = nullptr;
 }
 
-//END
+MapNode Map::getNodeTry(v3POS p)
+{
+#ifndef NDEBUG
+	ScopeProfiler sp(g_profiler, "Map: getNodeTry");
+#endif
+	auto blockpos = getNodeBlockPos(p);
+	auto block = getBlockNoCreateNoEx(blockpos, true);
+	if(!block)
+		return MapNode(CONTENT_IGNORE);
+	auto relpos = p - blockpos*MAP_BLOCKSIZE;
+	return block->getNodeTry(relpos);
+}
+
+/*
+MapNode Map::getNodeLog(v3POS p){
+	auto blockpos = getNodeBlockPos(p);
+	auto block = getBlockNoCreateNoEx(blockpos);
+	v3s16 relpos = p - blockpos*MAP_BLOCKSIZE;
+	auto node = block->getNodeNoEx(relpos);
+	infostream<<"getNodeLog("<<p<<") blockpos="<<blockpos<<" block="<<block<<" relpos="<<relpos<<" n="<<node<<std::endl;
+	return node;
+}
+*/
+
+/*
+MapNode Map::getNodeNoLock(v3s16 p) //dont use
+{
+	v3s16 blockpos = getNodeBlockPos(p);
+	MapBlock *block = getBlockNoCreateNoEx(blockpos);
+	if(block == NULL)
+		return MapNode(CONTENT_IGNORE);
+	return block->getNodeNoLock(p - blockpos*MAP_BLOCKSIZE);
+}
+*/
+v3POS Map::transforming_liquid_pop() {
+	std::lock_guard<std::mutex> lock(m_transforming_liquid_mutex);
+	auto front = m_transforming_liquid.front();
+	m_transforming_liquid.pop_front();
+	return front;
+
+	//auto lock = m_transforming_liquid.lock_unique_rec();
+	//auto it = m_transforming_liquid.begin();
+	//auto value = it->first;
+	//m_transforming_liquid.erase(it);
+	//return value;
+}
+
+s16 Map::getHeat(v3s16 p, bool no_random)
+{
+	MapBlock *block = getBlockNoCreateNoEx(getNodeBlockPos(p));
+	if(block != NULL) {
+		s16 value = block->heat;
+		return value + (no_random ? 0 : myrand_range(0, 1));
+	}
+	//errorstream << "No heat for " << p.X<<"," << p.Z << std::endl;
+	return 0;
+}
+
+s16 Map::getHumidity(v3s16 p, bool no_random)
+{
+	MapBlock *block = getBlockNoCreateNoEx(getNodeBlockPos(p));
+	if(block != NULL) {
+		s16 value = block->humidity;
+		return value + (no_random ? 0 : myrand_range(0, 1));
+	}
+	//errorstream << "No humidity for " << p.X<<"," << p.Z << std::endl;
+	return 0;
+}
+
+
+
+
+s16 ServerMap::updateBlockHeat(ServerEnvironment *env, v3POS p, MapBlock *block, std::map<v3POS, s16> * cache)
+{
+	auto bp = getNodeBlockPos(p);
+	auto gametime = env->getGameTime();
+	if (block) {
+		if (gametime < block->heat_last_update)
+			return block->heat + myrand_range(0, 1);
+	} else if (!cache) {
+		block = getBlockNoCreateNoEx(bp, true);
+	}
+	if (cache && cache->count(bp))
+		return cache->at(bp) + myrand_range(0, 1);
+
+	auto value = m_emerge->biomemgr->calcBlockHeat(p, getSeed(),
+			env->getTimeOfDayF(), gametime * env->getTimeOfDaySpeed(), env->m_use_weather);
+
+	if(block) {
+		block->heat = value;
+		block->heat_last_update = env->m_use_weather ? gametime + 30 : -1;
+	}
+	if (cache)
+		(*cache)[bp] = value;
+	return value + myrand_range(0, 1);
+}
+
+s16 ServerMap::updateBlockHumidity(ServerEnvironment *env, v3POS p, MapBlock *block, std::map<v3POS, s16> * cache)
+{
+	auto bp = getNodeBlockPos(p);
+	auto gametime = env->getGameTime();
+	if (block) {
+		if (gametime < block->humidity_last_update)
+			return block->humidity + myrand_range(0, 1);
+	} else if (!cache) {
+		block = getBlockNoCreateNoEx(bp, true);
+	}
+	if (cache && cache->count(bp))
+		return cache->at(bp) + myrand_range(0, 1);
+
+	auto value = m_emerge->biomemgr->calcBlockHumidity(p, getSeed(),
+			env->getTimeOfDayF(), gametime * env->getTimeOfDaySpeed(), env->m_use_weather);
+
+	if(block) {
+		block->humidity = value;
+		block->humidity_last_update = env->m_use_weather ? gametime + 30 : -1;
+	}
+	if (cache)
+		(*cache)[bp] = value;
+	return value + myrand_range(0, 1);
+}
+
+int ServerMap::getSurface(v3s16 basepos, int searchup, bool walkable_only) {
+
+	s16 max = MYMIN(searchup + basepos.Y,0x7FFF);
+
+	MapNode last_node = getNodeNoEx(basepos);
+	MapNode node = last_node;
+	v3s16 runpos = basepos;
+	INodeDefManager *nodemgr = m_gamedef->ndef();
+
+	bool last_was_walkable = nodemgr->get(node).walkable;
+
+	while ((runpos.Y < max) && (node.param0 != CONTENT_AIR)) {
+		runpos.Y += 1;
+		last_node = node;
+		node = getNodeNoEx(runpos);
+
+		if (!walkable_only) {
+			if ((last_node.param0 != CONTENT_AIR) &&
+				(last_node.param0 != CONTENT_IGNORE) &&
+				(node.param0 == CONTENT_AIR)) {
+				return runpos.Y;
+			}
+		}
+		else {
+			bool is_walkable = nodemgr->get(node).walkable;
+
+			if (last_was_walkable && (!is_walkable)) {
+				return runpos.Y;
+			}
+			last_was_walkable = is_walkable;
+		}
+	}
+
+	return basepos.Y -1;
+}
+
+
+INodeDefManager* Map::getNodeDefManager() {
+	return m_gamedef->ndef();
+}
