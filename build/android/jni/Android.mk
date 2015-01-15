c10f2c15625efb2a9da0579136009a1542b3e82d
proller <proller@github.com>
2015-01-15 21:47:29 +0300

    redice merge conflicts in defaultsettings.cpp

--- build/android/jni/Android.mk
+++ build/android/jni/Android.mk
@@ -125,7 +125,7 @@ LOCAL_SRC_FILES :=                                \
 		jni/src/FMColoredString.cpp               \
 		jni/src/FMStaticText.cpp                  \
 		jni/src/fmbitset.cpp                      \
-		jni/src/fmliquid.cpp                      \
+		jni/src/fm_liquid.cpp                      \
 		jni/src/intlGUIEditBox.cpp                \
 		jni/src/key_value_storage.cpp             \
 		jni/src/log_types.cpp                     \
