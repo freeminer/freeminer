2c52e1696100720de5f9f379954dd8d37478d3d4
proller <proller@github.com>
2015-01-29 00:07:42 +0300

    autotest fixes

--- util/autotest/autotest.sh
+++ util/autotest/autotest.sh
@@ -1,6 +1,10 @@
+#!/bin/sh
+
+# sudo apt-get install valgrind clang
+
 cmake_opt=-DBUILD_SERVER=0
 confdir=`pwd`
-run_opts="--config $confdir/freeminer.headless.conf --worldname autotest --go --autoexit 100"
+run_opts="--config $confdir/freeminer.headless.conf --worldname autotest --port 63000 --go --autoexit 100"
 logdir=`pwd`/logs
 
 cd ../..
@@ -16,33 +20,33 @@ name=asan
 mkdir -p _$name && cd _$name
 cmake $rootdir  -DCMAKE_CXX_COMPILER=`which clang++` -DCMAKE_C_COMPILER=`which clang` -DDISABLE_LUAJIT=1 -DSANITIZE_ADDRESS=1 -DDEBUG=1  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=`pwd` $cmake_opt
 time nice make -j $(nproc || sysctl -n hw.ncpu || echo 2)
-./freeminer $run_opts --logfile $logdir/autotest.$name.game.log >> $logdir/autotest.$name.out.log 2>>$logdir/autotest.$name.err.log
+nice ./freeminer $run_opts --logfile $logdir/autotest.$name.game.log >> $logdir/autotest.$name.out.log 2>>$logdir/autotest.$name.err.log
 cd ..
 
 name=tsan
 mkdir -p _$name && cd _$name
 cmake $rootdir  -DCMAKE_CXX_COMPILER=`which clang++` -DCMAKE_C_COMPILER=`which clang` -DDISABLE_LUAJIT=1 -DSANITIZE_THREAD=1  -DDEBUG=1  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=`pwd` $cmake_opt
 time nice make -j $(nproc || sysctl -n hw.ncpu || echo 2)
-./freeminer $run_opts --logfile $logdir/autotest.$name.game.log >> $logdir/autotest.$name.out.log 2>>$logdir/autotest.$name.err.log
+nice ./freeminer $run_opts --logfile $logdir/autotest.$name.game.log >> $logdir/autotest.$name.out.log 2>>$logdir/autotest.$name.err.log
 cd ..
 
 name=valgrind
 mkdir -p _$name && cd _$name
 cmake $rootdir -DDISABLE_LUAJIT=1 -DDEBUG=1  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=`pwd` $cmake_opt
 time nice make -j $(nproc || sysctl -n hw.ncpu || echo 2)
-valgrind ./freeminer $run_opts --logfile $logdir/autotest.$name.game.log >> $logdir/autotest.$name.out.log 2>>$logdir/autotest.$name.err.log
+nice valgrind ./freeminer $run_opts --logfile $logdir/autotest.$name.game.log >> $logdir/autotest.$name.out.log 2>>$logdir/autotest.$name.err.log
 cd ..
 
 name=nothreads
 mkdir -p _$name && cd _$name
 cmake $rootdir -DENABLE_THREADS=0 -DHAVE_THREAD_LOCAL=0 -DHAVE_FUTURE=0 -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=`pwd` $cmake_opt
 time nice make -j $(nproc || sysctl -n hw.ncpu || echo 2)
-./freeminer $run_opts --logfile $logdir/autotest.$name.game.log >> $logdir/autotest.$name.out.log 2>>$logdir/autotest.$name.err.log
+nice ./freeminer $run_opts --logfile $logdir/autotest.$name.game.log >> $logdir/autotest.$name.out.log 2>>$logdir/autotest.$name.err.log
 cd ..
 
 name=normal
 mkdir -p _$name && cd _$name
 cmake $rootdir -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=`pwd` $cmake_opt
 time nice make -j $(nproc || sysctl -n hw.ncpu || echo 2)
-./freeminer $run_opts --logfile $logdir/autotest.$name.game.log >> $logdir/autotest.$name.out.log 2>>$logdir/autotest.$name.err.log
+nice ./freeminer $run_opts --logfile $logdir/autotest.$name.game.log >> $logdir/autotest.$name.out.log 2>>$logdir/autotest.$name.err.log
 cd ..
