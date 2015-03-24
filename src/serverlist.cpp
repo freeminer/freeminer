9db3d0d00e33d9dad85246bcd13e51bb2aad00c8
proller <proller@github.com>
2015-03-24 21:17:40 +0300

    announce fix

--- src/serverlist.cpp
+++ src/serverlist.cpp
@@ -244,16 +244,17 @@ void sendAnnounce(const std::string &action,
 	fetch_request.timeout = fetch_request.connect_timeout = 59000;
 	fetch_request.url = g_settings->get("serverlist_url") + std::string("/announce");
 
+#if !MINETEST_PROTO
+	// todo: need to patch masterserver script to parse multipart posts
 	std::string query = std::string("json=") + urlencode(writer.write(server));
 	if (query.size() < 1000)
 		fetch_request.url += "?" + query;
 	else
 		fetch_request.post_data = query;
-
-/*
+#else
 	fetch_request.post_fields["json"] = writer.write(server);
 	fetch_request.multipart = true;
-*/
+#endif
 
 	httpfetch_async(fetch_request);
 #endif
