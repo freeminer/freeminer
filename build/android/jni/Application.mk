a3e4f9f8dcd23bebc17c3672d627148c76768acf
proller <proller@github.com>
2015-10-16 15:50:37 +0300

    android build fix

--- build/android/jni/Application.mk
+++ build/android/jni/Application.mk
@@ -5,7 +5,8 @@ APP_MODULES := freeminer
 APP_STL := gnustl_static
 
 #NDK_TOOLCHAIN_VERSION := clang3.6      # broken curl
-#APP_STL := c++_static
 
-APP_CPPFLAGS += -std=c++0x -fexceptions
+APP_STL := c++_shared
+
+APP_CPPFLAGS += -std=c++11 -fexceptions
 APP_GNUSTL_FORCE_CPP_FEATURES := rtti
