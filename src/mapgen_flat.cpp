fb82a6bbccaa78d974685a0d801b0c33ac66ed08
proller <proller@github.com>
2015-11-12 15:09:05 +0300

    merge fix

--- src/mapgen_flat.cpp
+++ src/mapgen_flat.cpp
@@ -306,7 +306,7 @@ void MapgenFlat::makeChunk(BlockMakeData *data)
 
 	//printf("makeChunk: %dms\n", t.stop());
 
-	updateLiquid(&data->transforming_liquid, full_node_min, full_node_max);
+	updateLiquid(full_node_min, full_node_max);
 
 	if (flags & MG_LIGHT)
 		calcLighting(node_min - v3s16(0, 1, 0), node_max + v3s16(0, 1, 0),
