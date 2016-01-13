15fc4b0e6e58f4da2d20cce68011ca2092cc5211
proller <proller@github.com>
2016-01-13 21:11:06 +0300

    up

---
+++ util/merge_minetest.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cd ..
+
+git remote add minetest https://github.com/minetest/minetest.git
+git fetch --all
+git merge --no-edit minetest/master && git push
+#git submodule foreach --recursive "git pull||true"
+cd games
+
+cd pixture
+git checkout master
+git pull
+cd ..
+
+cd default
+git checkout master
+git pull
+git remote add minetest_game https://github.com/minetest/minetest_game.git
+git fetch --all
+git submodule foreach --recursive "git checkout master; git pull||true"
+git commit -a -m "up"
+git merge --no-edit minetest_game/master && git push
+
+cd ..
+git commit -a -m "up"
+git push
+
+git status
