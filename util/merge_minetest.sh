b020c6d745c396c0674786316160d2351e4945b4
proller <proller@github.com>
2016-01-26 21:40:59 +0300

    fix merge_minetest.sh

--- util/merge_minetest.sh
+++ util/merge_minetest.sh
@@ -5,8 +5,9 @@ cd ..
 git remote add minetest https://github.com/minetest/minetest.git
 git fetch --all
 git pull
-git merge --no-edit minetest/master | grep "Conflict" && exit
+( git merge --no-edit minetest/master | grep -i "conflict" ) && exit
 #git submodule foreach --recursive "git pull||true"
+
 cd games
 
 cd pixture
@@ -21,7 +22,8 @@ git remote add minetest_game https://github.com/minetest/minetest_game.git
 git fetch --all
 git submodule foreach --recursive "git checkout master; git pull||true"
 git commit -a -m "up"
-git merge --no-edit minetest_game/master && git push
+( git merge --no-edit minetest_game/master | grep -i "conflict" ) && exit
+git push
 
 cd ..
 git commit -a -m "up"
