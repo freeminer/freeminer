e721bfb89d9557d147e6d7821856e0f868586817
proller <proller@github.com>
2016-01-28 15:00:24 +0300

    fix merge_upstream.sh

--- util/merge_upstream.sh
+++ util/merge_upstream.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 cd ..
-
-git remote add upstream https://github.com/freeminer/freeminer.git 
+git remote add upstream https://github.com/freeminer/freeminer.git
 git fetch --all
 git pull
-git merge --no-edit upstream/master && git push
+( git merge --no-edit upstream/master | grep -i "conflict" ) && exit
+git push
 git submodule update --init --recursive
 git status
