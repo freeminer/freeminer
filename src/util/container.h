13d0388a4fc3b457c5b74e016910b3b0b4b6d096
proller <proller@github.com>
2016-03-13 03:39:07 +0300

    small optimize

--- src/util/container.h
+++ src/util/container.h
@@ -237,7 +237,7 @@ public:
 	{
 		//try_shared_lock lock(m_mutex);
 		MutexAutoLock lock(m_mutex);
-		return (m_queue.size() == 0);
+		return m_queue.empty();
 	}
 	bool empty_try()
 	{
@@ -245,7 +245,7 @@ public:
 		MutexAutoLock lock(m_mutex);
 		//if (!lock.owns_lock())
 		//	return 1;
-		return (m_queue.size() == 0);
+		return m_queue.empty();
 	}
 	unsigned int size() {
 		MutexAutoLock lock(m_mutex);
