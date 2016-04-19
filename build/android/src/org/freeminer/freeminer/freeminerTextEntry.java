3f63b88b00e3b9a0159cce42a0b7d51a421ed7e3
proller <proller@github.com>
2016-04-19 20:23:18 +0300

    android: auto keyboard

--- build/android/src/org/freeminer/freeminer/freeminerTextEntry.java
+++ build/android/src/org/freeminer/freeminer/freeminerTextEntry.java
@@ -11,10 +11,13 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.widget.EditText;
+import android.view.inputmethod.InputMethodManager;
+import android.content.Context;
 
 public class freeminerTextEntry extends Activity {
 	public AlertDialog mTextInputDialog;
 	public EditText mTextInputWidget;
+	public InputMethodManager imm;
 	
 	private final int MultiLineTextInput              = 1;
 	private final int SingleLineTextInput             = 2;
@@ -44,6 +47,7 @@ public class freeminerTextEntry extends Activity {
 		}
 		
 		
+
 		builder.setView(mTextInputWidget);
 		
 		if (editType == MultiLineTextInput) {
@@ -63,7 +67,6 @@ public class freeminerTextEntry extends Activity {
 			@Override
 			public boolean onKey(View view, int KeyCode, KeyEvent event) {
 				if ( KeyCode == KeyEvent.KEYCODE_ENTER){
-
 					pushResult(mTextInputWidget.getText().toString());
 					return true;
 				}
@@ -73,18 +76,25 @@ public class freeminerTextEntry extends Activity {
 		
 		mTextInputDialog = builder.create();
 		mTextInputDialog.show();
+
+		mTextInputWidget.requestFocus();
+		imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
 	}
 	
 	public void pushResult(String text) {
 		Intent resultData = new Intent();
 		resultData.putExtra("text", text);
 		setResult(Activity.RESULT_OK,resultData);
+		imm.hideSoftInputFromWindow(mTextInputWidget.getWindowToken(), 0);
 		mTextInputDialog.dismiss();
 		finish();
 	}
 	
 	public void cancelDialog() {
 		setResult(Activity.RESULT_CANCELED);
+		imm.hideSoftInputFromWindow(mTextInputWidget.getWindowToken(), 0);
 		mTextInputDialog.dismiss();
 		finish();
 	}
