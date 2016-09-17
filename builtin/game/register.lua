e70cf25139e0c97e5904476178d6406c16d62f70
proller <proller@github.com>
2016-09-17 16:12:45 +0300

    fix mt shit

--- builtin/game/register.lua
+++ builtin/game/register.lua
@@ -128,7 +128,7 @@ function core.register_item(name, itemdef)
 			}
 		end
 		if itemdef.light_source and itemdef.light_source > core.LIGHT_MAX then
-			error("Unable to register node: 'light_source' exceeds maximum: " .. name)
+			print("Unable to register node: 'light_source' exceeds maximum: " .. name)
 		end
 		setmetatable(itemdef, {__index = core.nodedef_default})
 		core.registered_nodes[itemdef.name] = itemdef
