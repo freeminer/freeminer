95bd21cf5daf9336301ef795c5a19956853b3671
proller <proller@github.com>
2016-09-30 17:45:59 +0300

    allow compiling sqlite from src/external/sqlite3 (for ugly OS)

--- cmake/Modules/FindSQLite3.cmake
+++ cmake/Modules/FindSQLite3.cmake
@@ -1,4 +1,6 @@
-mark_as_advanced(SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR) 
+mark_as_advanced(SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
+
+if (NOT SQLITE3_INCLUDE_DIR AND NOT SQLITE3_LIBRARY)
 
 find_path(SQLITE3_INCLUDE_DIR sqlite3.h)
 
@@ -7,3 +9,13 @@ find_library(SQLITE3_LIBRARY NAMES sqlite3)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SQLite3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
 
+endif()
+
+if (NOT SQLITE3_INCLUDE_DIR)
+	find_path(SQLITE3_INCLUDE_DIR sqlite3.h ${PROJECT_SOURCE_DIR}/external/sqlite3 NO_DEFAULT_PATH)
+	if (SQLITE3_INCLUDE_DIR)
+		add_subdirectory(${PROJECT_SOURCE_DIR}/external/sqlite3)
+		set(SQLITE3_LIBRARY sqlite3)
+		message(STATUS "Using bundled sqlite3 ${SQLITE3_INCLUDE_DIR} ${SQLITE3_LIBRARY}")
+	endif()
+endif()
