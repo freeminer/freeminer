371b8c04ce3428dd08849cf49f36b689b249fb32
proller <proller@github.com>
2016-10-01 18:47:23 +0300

    fix bundled sqlite build

--- cmake/Modules/FindSQLite3.cmake
+++ cmake/Modules/FindSQLite3.cmake
@@ -11,7 +11,7 @@ find_package_handle_standard_args(SQLite3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_IN
 
 endif()
 
-if (NOT SQLITE3_INCLUDE_DIR)
+if (NOT SQLITE3_INCLUDE_DIR OR NOT SQLITE3_LIBRARY)
 	find_path(SQLITE3_INCLUDE_DIR sqlite3.h ${PROJECT_SOURCE_DIR}/external/sqlite3 NO_DEFAULT_PATH)
 	if (SQLITE3_INCLUDE_DIR)
 		add_subdirectory(${PROJECT_SOURCE_DIR}/external/sqlite3)
