f505fbc0e1823481c7d1d4a4933cc3cfaaa41766
proller <proller@github.com>
2022-07-06 20:45:24 +0300

    Update jsoncpp to 1.9.5

--- cmake/Modules/FindJson.cmake
+++ cmake/Modules/FindJson.cmake
@@ -8,13 +8,13 @@ option(ENABLE_SYSTEM_JSONCPP "Enable using a system-wide JSONCPP.  May cause seg
 
 if(ENABLE_SYSTEM_JSONCPP)
 	find_library(JSON_LIBRARY NAMES jsoncpp)
-	find_path(JSON_INCLUDE_DIR json/features.h PATH_SUFFIXES jsoncpp)
+	find_path(JSON_INCLUDE_DIR json/allocator.h PATH_SUFFIXES jsoncpp)
 
 	include(FindPackageHandleStandardArgs)
 	find_package_handle_standard_args(JSONCPP DEFAULT_MSG JSON_LIBRARY JSON_INCLUDE_DIR)
 
 	if(JSONCPP_FOUND)
-		message(STATUS "Using system JSONCPP library.")
+		message(STATUS "Using system JSONCPP library ${JSON_LIBRARY} : ${JSON_INCLUDE_DIR}")
 	endif()
 endif()
 
@@ -25,10 +25,7 @@ if(NOT JSONCPP_FOUND)
 	option(JSONCPP_WITH_WARNING_AS_ERROR OFF)
 	add_subdirectory(external/jsoncpp)
 	message(STATUS "Using bundled JSONCPP library. ${jsoncpp_BINARY_DIR}")
-	set(JSON_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/external/jsoncpp/include)
+	set(JSON_INCLUDE_DIR BEFORE SYSTEM ${PROJECT_SOURCE_DIR}/external/jsoncpp/include)
 	set(JSON_LIBRARY jsoncpp_static)
-	#set(JSON_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/external/jsoncpp)
-	#set(JSON_LIBRARY jsoncpp)
-	#add_subdirectory(jsoncpp/json)
 endif()
 
