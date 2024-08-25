345fd7c4b3846a2d81c7be947ffc4db8a0a424b4
proller <proller@github.com>
2024-08-25 15:28:13 +0200

    fix

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -4632,7 +4632,6 @@ void Game::updateFrame(ProfilerGraph *graph, RunStats *stats, f32 dtime,
 #endif
 /* */
 
-#if 1
 	const auto camera_position = camera->getPosition();
 	if (!runData.headless_optimize)
 		if ((client->m_new_meshes &&
