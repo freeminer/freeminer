34e6a28d8008f769991ad49ef81e95cb4b5892bf
proller <proller@github.com>
2024-09-29 07:44:34 +0200

    fix

---
+++ src/client/fm_far_container.cpp
@@ -0,0 +1,53 @@
+#include "fm_far_container.h"
+#include "client.h"
+#include "client/clientmap.h"
+#include "client/fm_far_calc.h"
+#include "mapblock.h"
+#include "mapgen/mapgen.h"
+#include "mapnode.h"
+
+FarContainer::FarContainer(Client *client) : m_client{client}
+{
+}
+
+const MapNode &FarContainer::getNodeRefUnsafe(const v3pos_t &pos)
+{
+	auto bpos = getNodeBlockPos(pos);
+
+	int fmesh_step = getFarStep(m_client->getEnv().getClientMap().getControl(),
+			getNodeBlockPos(m_client->getEnv().getClientMap().m_far_blocks_last_cam_pos),
+			bpos);
+
+	const auto &shift = fmesh_step; // + cell_size_pow;
+	v3bpos_t bpos_aligned((bpos.X >> shift) << shift, (bpos.Y >> shift) << shift,
+			(bpos.Z >> shift) << shift);
+
+	if (const auto &it = far_blocks[fmesh_step].find(bpos_aligned);
+			it != far_blocks[fmesh_step].end()) {
+		const auto &block = it->second;
+		v3pos_t relpos = pos - bpos_aligned * MAP_BLOCKSIZE;
+
+		const auto &relpos_shift = fmesh_step; // + 1;
+		auto relpos_shifted = v3pos_t(relpos.X >> relpos_shift, relpos.Y >> relpos_shift,
+				relpos.Z >> relpos_shift);
+		const auto &n = block->getNodeNoLock(relpos_shifted);
+		if (n.getContent() != CONTENT_IGNORE) {
+			return n;
+		}
+	}
+
+	const auto &v = m_mg->visible_content(pos);
+	if (v.getContent())
+		return v;
+	return m_mg->visible_transparent;
+};
+
+MapNode FarContainer::getNodeNoExNoEmerge(const v3pos_t &p)
+{
+	return getNodeRefUnsafe(p);
+};
+
+MapNode FarContainer::getNodeNoEx(const v3pos_t &p)
+{
+	return getNodeRefUnsafe(p);
+};
---
+++ src/client/fm_far_container.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "fm_nodecontainer.h"
+#include "mapblock.h"
+#include "threading/concurrent_unordered_map.h"
+
+class Mapgen;
+class Client;
+class FarContainer : public NodeContainer
+{
+	Client *m_client;
+
+public:
+	Mapgen *m_mg{};
+
+	std::array<concurrent_unordered_map<v3bpos_t, MapBlockP>, FARMESH_STEP_MAX>
+			far_blocks;
+
+	FarContainer(Client *client);
+	const MapNode &getNodeRefUnsafe(const v3pos_t &p) override;
+	MapNode getNodeNoExNoEmerge(const v3pos_t &p) override;
+	MapNode getNodeNoEx(const v3pos_t &p) override;
+};
