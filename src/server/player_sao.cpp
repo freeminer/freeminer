4a6057c6a1ebadb688113c5dfa71a025233bda9a
proller <proller@github.com>
2024-12-27 02:44:04 +0100

    better

--- src/server/player_sao.cpp
+++ src/server/player_sao.cpp
@@ -620,9 +620,9 @@ std::string PlayerSAO::getPropertyPacket()
 void PlayerSAO::setMaxSpeedOverride(const v3f &vel)
 {
 	if (m_max_speed_override_time == 0.0f)
-		m_max_speed_override = vel;
+		m_max_speed_override = v3fToOpos(vel);
 	else
-		m_max_speed_override += vel;
+		m_max_speed_override += v3fToOpos(vel);
 	if (m_player) {
 		float accel = MYMIN(m_player->movement_acceleration_default,
 				m_player->movement_acceleration_air);
