1ee2ba5046429660c608cadbb34b26209100ecac
proller <proller@github.com>
2026-05-11 02:27:01 +0200

    32bit fixes

--- src/client/mcp_player_control.h
+++ src/client/mcp_player_control.h
@@ -1,30 +1,32 @@
 #pragma once
 
 #include "client.h"
+#include "irrlichttypes.h"
 #include "localplayer.h"
 #include <json/json.h>
 #include <signal.h>
 
-class MCPPlayerControl {
+class MCPPlayerControl
+{
 public:
-    MCPPlayerControl(Client* client);
-    ~MCPPlayerControl();
-    
-    // Update player state from MCP server
-    void updatePlayerState();
-    
-    // Control methods
-    void setPlayerControl(const PlayerControl& control);
-    void movePlayerTo(float x, float y, float z);
-    void rotatePlayer(float pitch, float yaw);
-    void teleportPlayer(float x, float y, float z);
-    
+	MCPPlayerControl(Client *client);
+	~MCPPlayerControl();
+
+	// Update player state from MCP server
+	void updatePlayerState();
+
+	// Control methods
+	void setPlayerControl(const PlayerControl &control);
+	void movePlayerTo(opos_t x, opos_t y, opos_t z);
+	void rotatePlayer(float pitch, float yaw);
+	void teleportPlayer(float x, float y, float z);
+
 private:
-    bool initializeMCP();
-    Json::Value sendMCPRequest(const std::string& method, const Json::Value& params);
-    
-    Client* m_client;
-    pid_t m_mcp_process = 0;
-    int m_mcp_stdin = -1;
-    int m_mcp_stdout = -1;
+	bool initializeMCP();
+	Json::Value sendMCPRequest(const std::string &method, const Json::Value &params);
+
+	Client *m_client;
+	pid_t m_mcp_process = 0;
+	int m_mcp_stdin = -1;
+	int m_mcp_stdout = -1;
 };
