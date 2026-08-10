299898d37493ca5ab93b2927f6f011d756b65423
SmallJoker <mk939@ymail.com>
2026-08-10 20:53:49 +0200

    luantiserver: Get --debugger to work
    '--console' does not exist when ENABLE_CLIENT=0, which then
    causes the engine to print the help dialogue.

--- src/main.cpp
+++ src/main.cpp
@@ -716,7 +716,9 @@ static bool use_debugger(int argc, char *argv[])
 			continue;
 		new_args.push_back(argv[i]);
 	}
+#if CHECK_CLIENT_BUILD()
 	new_args.push_back("--console");
+#endif
 	new_args.push_back(nullptr);
 
 #ifdef _WIN32
