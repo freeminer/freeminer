9210b42f7f8c4a22fd5cd9dae8162197cc3ab436
proller <proller@github.com>
2026-08-24 23:49:05 +0200

    git submodule update --remote

--- util/merge_upstream.sh
+++ util/merge_upstream.sh
@@ -6,5 +6,5 @@ git fetch --all
 git pull
 ( git merge --no-edit upstream/master | grep -i "conflict" ) && exit
 git push
-git submodule update --init --recursive
+git submodule update --init --recursive --remote
 git status
